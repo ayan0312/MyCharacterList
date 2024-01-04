fdcf5abde71a3633b23f784137e8c95f319c2ef0
jg0312 <ayan0312000@outlook.com>
2024-01-05 00:16:34 +0800

    feat: add theme generator

--- src/drawer/settings/DrawerOptions.vue
+++ src/drawer/settings/DrawerOptions.vue
@@ -3,6 +3,7 @@
         <settings-header title="theme" text="theme-message" />
         <theme-option />
         <settings-header title="custom-theme" text="more-coming-soon" />
+        <theme-generator />
         <v-divider class="mt-4 mb-3" />
         <settings-header title="general" text="general-message" />
     </div>
@@ -11,4 +12,5 @@
 <script setup lang="ts">
 import ThemeOption from './options/Theme.vue'
 import SettingsHeader from './SettingsHeader.vue'
+import ThemeGenerator from './options/ThemeGenerator.vue'
 </script>
---
+++ src/drawer/settings/options/ThemeGenerator.vue
@@ -0,0 +1,18 @@
+<template>
+    <app-link-list-item
+        href="https://theme-generator.vuetifyjs.com/"
+        :title="t('open')"
+        :label="t('theme-generator')"
+        append-icon="mdi-open-in-new"
+        prepend-icon="mdi-format-color-fill"
+        rel="noopener noreferrer"
+        target="_blank"
+        min-width="90"
+    />
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
